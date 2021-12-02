37f8f01c828fd730a81e43f9d7dc19340b24b91c
Rajesh Perakalapudi <perakalapudi.rajesh@nokia.com>
2021-12-02 10:44:35 +0530

    added java source

---
+++ src/com/jenkins/init/Start.java
@@ -0,0 +1,12 @@
+package com.jenkins.init;
+
+public class Start {
+	public static void main(String[] args){
+		
+		System.out.println(" Java Program Started ");
+	}
+	
+	public int Add(int x,int y){
+		return x+y;
+	}
+}
