49a1e2cbbdbaa400cd5bf3c458eb80457b69be9a
Rajesh Perakalapudi <perakalapudi.rajesh@nokia.com>
2021-12-21 19:38:45 +0530

    Error added to java code

--- src/com/jenkins/init/Start.java
+++ src/com/jenkins/init/Start.java
@@ -4,6 +4,10 @@ public class Start {
 	public static void main(String[] args){
 		
 		System.out.println(" Java Program Started ");
+		int x=0;
+		int y=5/0;
+		int z=10/x;
+		System.out.println(" Value of Y "+y+" and z is "+z);
 	}
 	
 	public static int Add(int x,int y){
