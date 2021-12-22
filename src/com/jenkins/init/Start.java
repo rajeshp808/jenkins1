f93889383c8fb8ca991a402c23b2ad3f4363b5c1
Rajesh Perakalapudi <perakalapudi.rajesh@nokia.com>
2021-12-22 20:11:13 +0530

    Code experiments closed.. Old code restored

--- src/com/jenkins/init/Start.java
+++ src/com/jenkins/init/Start.java
@@ -6,8 +6,7 @@ public class Start {
 		System.out.println(" Java Program Started ");
 		
 		int y=0;
-		int z=10/y;
-		int iData=z/0;
+		int z=10/2;
 		
 		System.out.println(" Value of Y "+y+" and z is "+z);
 		String strProject="Hello";
